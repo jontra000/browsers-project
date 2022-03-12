1265707077dbd422ebb9c4c8b0fc252cf41c38c3
Huseyin Yaman <husoyaman5@gmail.com>
2022-03-12 01:43:58 +0100

    Second Commit

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -29,10 +29,131 @@ export const initQuestionPage = () => {
   document
     .getElementById(NEXT_QUESTION_BUTTON_ID)
     .addEventListener('click', nextQuestion);
+  
+ //adding event listener on click
+ document
+ .getElementById(ANSWERS_LIST_ID)
+ .addEventListener('click', checkAnswer);
+
+ //adding event listener on click to save selected answer
+  document
+  .getElementById(ANSWERS_LIST_ID)
+  .addEventListener('click', saveAnswer);
 };
+
+
+let currentQuestion = quizData.questions[quizData.currentQuestionIndex];
+let userAnswers = [];
+console.log(userAnswers);
+let score = 0;
+const timeValue = 15;
+let counter;
 
 const nextQuestion = () => {
-  quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
+  quizData.currentQuestionIndex++;
+  // console.log(currentQuestionIndex);
+  currentQuestion = quizData.questions[quizData.currentQuestionIndex];
+  console.log(currentQuestion);
 
   initQuestionPage();
+  startTimer(timeValue);
 };
+
+//Checks if selected answer is correct
+const checkAnswer = (evt) => {
+  const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
+  const value = evt.target.value;
+  const ull = document.getElementById(ANSWERS_LIST_ID);
+  ull.style.pointerEvents = 'none';
+  document.getElementById(NEXT_QUESTION_BUTTON_ID).style.visibility = 'visible';
+
+  clearInterval(counter);
+  
+  userAnswers.push(value);
+ 
+   if (value === currentQuestion.correct) {
+     answerIsCorrect();
+   } else {
+     answerIsWrong();
+   }
+   //if answer is correct
+ function answerIsCorrect() {
+  score++;
+  evt.target.style.backgroundColor = "green";
+  
+  // alert(`Correct! Your current score is ${score}`);
+}
+
+//if answer is wrong
+function answerIsWrong() {
+    score;
+    evt.target.style.backgroundColor = "red";
+    showCorrectAnswer();
+    // alert(`You will get it right next time! Your current score is ${score}`);
+  }
+}
+
+const showCorrectAnswer = () => {
+  const buttons = document.querySelectorAll('button')
+  // quizData.currentQuestionIndex++;
+  // console.log(currentQuestion);
+  
+    buttons.forEach(button => {
+      if (button.value === currentQuestion.correct){
+        button.style.backgroundColor = "green";
+      }
+    })
+}
+
+
+
+//saves user's selected answers 
+const saveAnswer = (e) => {
+  const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
+  const value = e.target.value;
+  const answerObj = {
+    'Question': quizData.currentQuestionIndex + 1,
+    'Answer' : value
+  }
+  userAnswers.push(answerObj);
+  localStorage.setItem("userAnswers", JSON.stringify(userAnswers));
+}
+
+//retrieves user's answers on page reload
+window.onload = function() {
+  if (localStorage.getItem('userAnswers')) {
+    userAnswers = JSON.parse(localStorage.getItem('userAnswers'));
+}}
+ 
+ 
+// Timer
+
+export function startTimer(time) {
+  
+  counter = setInterval(timer, 1000);
+  function timer() {
+    const timeCount = document.querySelector('#timeCount')
+    timeCount.textContent = time; 
+    time--; 
+
+    if (time < 9) {
+      
+      let addZero = timeCount.textContent;
+      timeCount.textContent = '0' + addZero; 
+    }
+    if (time < 0) {
+      
+      clearInterval(counter); 
+      timeCount.textContent = 'Time Off'; 
+      showCorrectAnswer();
+      document.getElementById(NEXT_QUESTION_BUTTON_ID).style.visibility = 'visible';
+
+      const ull = document.getElementById(ANSWERS_LIST_ID);
+      ull.style.pointerEvents = 'none';
+    }
+  }
+}
+
+
+
+
--- src/pages/welcomePage.js
+++ src/pages/welcomePage.js
@@ -2,7 +2,11 @@
 
 import { USER_INTERFACE_ID, START_QUIZ_BUTTON_ID } from '../constants.js';
 import { createWelcomeElement } from '../views/welcomeView.js';
-import { initQuestionPage } from './questionPage.js';
+import { initQuestionPage, startTimer } from './questionPage.js';
+
+const info_box = document.querySelector('.info_box');
+const exit_btn = info_box.querySelector('.quit');
+const continue_btn = info_box.querySelector('.restart');
 
 export const initWelcomePage = () => {
   const userInterface = document.getElementById(USER_INTERFACE_ID);
@@ -16,6 +20,25 @@ export const initWelcomePage = () => {
     .addEventListener('click', startQuiz);
 };
 
+// const startQuiz = () => {
+//   startTimer(15);
+//   initQuestionPage();
+  
+// };
+
 const startQuiz = () => {
-  initQuestionPage();
+  info_box.classList.add('activeInfo');       // Show info box
+  
 };
+
+exit_btn.addEventListener('click', () => {
+  info_box.classList.remove('activeInfo');    // Hide info box
+});
+
+continue_btn.addEventListener('click', () => {
+  info_box.classList.remove('activeInfo'); //hide info box
+  startTimer(15);
+  initQuestionPage();
+  // quiz_box.classList.add('activeQuiz'); //show quiz box
+  // showQuestions(0); //calling showQestions function (first question)
+});
